148dde3864ab731a5356c2d3c0dfea25efd65644
Александр <aleksandr@Mac-mini-Aleksandr.local>
2025-06-28 22:35:15 +0300

    update run.py and add client.py

--- run.py
+++ run.py
@@ -3,16 +3,15 @@ import asyncio
 import logging
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message
-from aiogram.filters import CommandStart
 
 from app.handlers import router
+from app.client import client
 
 async def main():
     load_dotenv()
     bot = Bot(token=os.getenv('TOKEN')) # type: ignore
     dp = Dispatcher()
-    dp.include_router(router)
+    dp.include_routers(router, client)
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
@@ -21,4 +20,4 @@ if __name__ == '__main__':
         print('Бот включен')
         asyncio.run(main())
     except KeyboardInterrupt:
-        print('Бот выключен')
+        logging.info('Бот выключен')
